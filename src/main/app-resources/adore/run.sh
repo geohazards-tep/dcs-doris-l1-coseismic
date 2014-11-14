4c90e598e1f473420d7594f921a98f14a6c4e18f
fdandria <fdandria@sb-10-16-10-30.dev.terradue.int>
2014-11-14 18:07:09 +0100

    [devel] updated application and run

--- src/main/app-resources/adore/run.sh
+++ src/main/app-resources/adore/run.sh
@@ -29,15 +29,22 @@ function cleanExit ()
 trap cleanExit EXIT
 
 # path and master/slave variable definition
-UUIDTMP="${TMPDIR}/`uuidgen`"
+UUID=`uuidgen`
+#UUIDTMP="${TMPDIR}/`uuidgen`"
+UUIDTMP="/tmp/fabio/${UUID}"
 MASTER="`ciop-getparam adore_master`"
-SLAVE="`ciop-getparam adore_slave`"
+PROJECT="`ciop-getparam adore_project`"
+SHORTPATH=/tmp/${UUID}
 
 # creates the adore directory structure
 ciop-log "INFO" "creating the directory structure"
-mkdir ${UUIDTMP}/data/
+mkdir -p ${UUIDTMP}
+ln -s ${UUIDTMP} ${SHORTPATH}
+mkdir ${UUIDTMP}/data
 mkdir ${UUIDTMP}/data/master
 mkdir ${UUIDTMP}/data/slave
+
+ciop-log "INFO" "basedir is ${UUIDTMP} ${SHORTPATH}"
 
 # copies the ODR files
 ciop-log "INFO" "copying the ODR files"
@@ -45,24 +52,58 @@ cd ${UUIDTMP}/
 tar xvfz /application/adore/files/ODR.tgz
 
 # retrieves the files
-ciop-log "INFO" "retrieving master and slave"
+ciop-log "INFO" "retrieving master [$MASTER]"
 cd ${UUIDTMP}/data/
-ciop-copy -O ${UUIDTMP}/master ${MASTER}
+
+#test mode
+if [ ! -e "/var/lib/hadoop-0.20/`basename ${MASTER}`" ]
+then
+	ciop-log "INFO" "downloading master [${MASTER}]"
+	#ciop-copy -O ${UUIDTMP}/data/master ${MASTER}
+	cd /var/lib/hadoop-0.20/
+	curl -O ${MASTER} 2> /dev/null
+	cd -
+	#ciop-copy -O ${UUIDTMP}/data/master ${MASTER}
+fi
+
+cd ${UUIDTMP}/data/master/
+unzip /var/lib/hadoop-0.20/`basename ${MASTER}`
+cd -
+
 res=$?
-ciop-copy -O ${UUIDTMP}/slave ${SLAVE}
-res=$(( $res + $? ))
+
+while read input
+do
+	ciop-log "INFO" "retrieving slave [$input]"
+	#ciop-copy -O ${UUIDTMP}/data/slave ${input}
+	if [ ! -e "/var/lib/hadoop-0.20/`basename ${input}`" ]
+	then
+		ciop-log "INFO" "downloading file"
+		cd /var/lib/hadoop-0.20/
+		curl -O $input
+		cp `basename $input`  ${UUIDTMP}/data/slave/
+		cd -
+	fi
+
+	cd ${UUIDTMP}/data/slave/
+	unzip /var/lib/hadoop-0.20/`basename ${input}`
+	cd -
+
+	res=$(( $res + $? ))
+done
 
 if [ $res -ne 0 ]; then exit $ERR_CURL; fi
 
 # setting the adore settings.set file
-cat /application/adore/files/settings.set.template | sed "s|#BASEDIR#|${UUIDTMP}|g" > ${UUIDTMP}/settings.set
+cat /application/adore/files/settings.set.template | sed "s|#BASEDIR#|${SHORTPATH}|g" | sed "s|#PROJECT#|${PROJECT}|g" > ${UUIDTMP}/settings.set
 
 # ready to lauch adore
 cd ${UUIDTMP}
-adore -u settings.set "m_readfiles; settings apply -r m_orbdir=${UUIDTMP}/ODR; m_porbits; s_readfiles; s_porbits; m_crop; s_crop; coarseorb; dem make SRTM3 50 LAquila; s raster_format; settings apply -r raster_format=png; raster a m_crop -- -M1/5; raster a s_crop -- -M1/5; m_simamp; m_timing; coarsecorr; fine; reltiming; demassist; coregpm; resample; interfero; comprefpha; subtrrefpha; comprefdem; subtrrefdem; coherence; raster p subtrrefdem -- -M4/4; raster p subtrrefpha -- -M4/4; raster p interfero -- -M4/4; raster p coherence -- -M4/4 -cgray -b"
+export ADORESCR=/opt/adore/scr; export PATH=${PATH}:${ADORESCR}:/usr/local/bin
+adore -u settings.set "m_readfiles; settings apply -r m_orbdir=${SHORTPATH}}/ODR; m_porbits; s_readfiles; s_porbits; m_crop; s_crop; coarseorb; dem make SRTM3 50 LAquila; s raster_format; settings apply -r raster_format=png; raster a m_crop -- -M1/5; raster a s_crop -- -M1/5; m_simamp; m_timing; coarsecorr; fine; reltiming; demassist; coregpm; resample; interfero; comprefpha; subtrrefpha; comprefdem; subtrrefdem; coherence; raster p subtrrefdem -- -M4/4; raster p subtrrefpha -- -M4/4; raster p interfero -- -M4/4; raster p coherence -- -M4/4 -cgray -b"
 
-ciop-publish -m ${UUIDTMP}/*
+ciop-publish -m ${UUIDTMP}/*.png
 
-rm -rf ${UUIDTMP}
+#rm -rf ${UUIDTMP}
 
 ciop-log "INFO" "That's all folks"
